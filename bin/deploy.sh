94aeecb14a98ce6059ce9b8a12933002207af1a5
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-11-04 12:30:37 +0100

    Make deploy script throw when used wrong

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,12 @@
+if [ -d .git ]; then
+  echo "deploy.sh needs to be run from within the app folder you want to deploy. E.g. cd first-app; ../bin/deploy.sh"
+  exit 1
+fi
+
 elm-app build
 APP=$(basename $PWD)
 mv dist ../docs/$APP
+
 git reset
 git add ../docs
 git ci -m "Deploy $APP"
