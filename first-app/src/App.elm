644b944cfe0d452b21714693b04b78710114fb5f
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-11-04 14:34:36 +0100

    Make an http request and decode it..

--- first-app/src/App.elm
+++ first-app/src/App.elm
@@ -1,10 +1,18 @@
 module App exposing (..)
 
 import Html exposing (text, div)
+import Json.Decode as Json exposing ((:=))
+import Task exposing (..)
+import Http exposing (..)
+
+
+type alias Event =
+    { id : String }
 
 
 type alias Entry =
-    { user : String
+    { id : String
+    , user : User
     , action : String
     }
 
@@ -13,16 +21,29 @@ type alias Entries =
     List Entry
 
 
+type alias User =
+    String
+
+
+type alias Users =
+    List User
+
+
+users =
+    [ "torgeir", "emilmork" ]
+
+
 initialEntries =
-    [ { user = "@torgeir", action = "forked <repo> to <forked-repo>" }
-    , { user = "@emilmork", action = "starred <repo>" }
+    [ { id = "n-a", user = "torgeir", action = "forked <repo> to <forked-repo>" }
+    , { id = "n-a", user = "emilmork", action = "starred <repo>" }
     ]
 
 
 entryView : Entry -> Html.Html Msg
 entryView entry =
     div []
-        [ text entry.user
+        [ text entry.id
+        , text entry.user
         , text " "
         , text entry.action
         ]
@@ -34,6 +55,20 @@ type alias Model =
 
 type Msg
     = String
+    | OkMsg (List Event)
+    | FailedMsg Http.Error
+
+
+decoder : Json.Decoder (List Event)
+decoder =
+    Json.list decoderEvent
+
+
+decoderEvent : Json.Decoder Event
+decoderEvent =
+    Json.object1
+        Event
+        ("id" := Json.string)
 
 
 subscriptions : Model -> Sub Msg
@@ -41,14 +76,27 @@ subscriptions model =
     Sub.none
 
 
+eventToEntry : Event -> Entry
+eventToEntry event =
+    { id = event.id
+    , action = ""
+    , user = ""
+    }
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        OkMsg eventList ->
+            ( (List.map eventToEntry eventList), Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialEntries, Cmd.none )
+    ( initialEntries, (Task.perform FailedMsg OkMsg (Http.get decoder "https://api.github.com/users/torgeir/events/public")) )
 
 
 view : Model -> Html.Html Msg
